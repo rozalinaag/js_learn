6e5d77bc55056b7d028a09dab6eaea7bb317e2ca
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-12-28 13:31:44 -0500

    show more button

--- 11-inline-properties/app.js
+++ 11-inline-properties/app.js
@@ -1,2 +1,12 @@
 let content = document.getElementById('content');
 let button = document.getElementById('show-more');
+
+button.onclick = () => {
+  if (content.className == '') {
+    content.className = 'open';
+    button.innerHTML = 'Collapse now';
+  } else {
+    content.className = '';
+    button.innerHTML = 'Press Me and Show More';
+  }
+};
