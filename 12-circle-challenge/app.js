f8b9e7d30bad74d16878b92e4cddf286ff98880f
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-12-30 15:13:09 -0500

    click circle

---
+++ 12-circle-challenge/app.js
@@ -0,0 +1,12 @@
+//step 1: add text dynamically to the div
+document.getElementById('note').textContent = 'this is Dom manipulation';
+
+let circle = document.getElementById('circle');
+circle.addEventListener('click', changeColor);
+
+function changeColor(event) {
+  //toScring(16) - translate to hexadecimal value
+  let randomColor = Math.floor(Math.random() * 16777216).toString(16);
+  console.log(randomColor);
+  circle.style.backgroundColor = `#${randomColor}`;
+}
