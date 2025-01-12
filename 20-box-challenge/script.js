13f5eabb922bbfd0d3e63754854d94da5a83425a
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-01-11 21:44:59 -0500

    random color

--- 20-box-challenge/script.js
+++ 20-box-challenge/script.js
@@ -5,17 +5,25 @@ let selectedElement;
 
 function onClickTable(e) {
   let item = e.target;
-  highlight(item);
 
-  item.classList.add('highlight');
+  if (item.tagName != 'TD') {
+    return;
+  } else {
+    highlight(item);
+  }
 }
 
 function highlight(node) {
   if (selectedElement != null) {
-    selectedElement.classList.remove('highlight');
+    // selectedElement.classList.remove('highlight');
+    selectedElement.style.backgroundColor = `gray`;
   }
 
   //assign the new TD element we've clicked to the selectElement variable
   selectedElement = node;
-  selectedElement.classList.add('highlight');
+  // selectedElement.classList.add('highlight');
+
+  let randomColor = Math.floor(Math.random() * 16777216).toString(16);
+  randomColor = randomColor.padStart(6, '0');
+  selectedElement.style.backgroundColor = `#${randomColor}`;
 }
